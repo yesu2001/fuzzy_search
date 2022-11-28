b6196d2d4430ad4337d310c9f65c961b0e06781f
Yesu <yesupadam2001@gmail.com>
2022-11-28 23:09:17 +0530

    first commit

---
+++ chatbot-master/index.js
@@ -0,0 +1,131 @@
+const lists = document.querySelector('#patterns');
+const list = document.querySelector('#patterns')
+const inputField = document.getElementById("input");
+
+
+const setList = (words) => {
+  // loop through the array of strings
+  for(const word of words) {
+    // create an li tag
+    const resultItem = document.createElement('li')
+    // add class name to the li
+    resultItem.classList.add('result-item');
+    // takes word from array 
+    const text = document.createTextNode(word)
+    // adds the text to the li tag
+    resultItem.appendChild(text)
+    // then li's are added to the ul
+    lists.appendChild(resultItem)
+  }
+  
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  inputField.addEventListener("keyup", (e) => {
+    let input = inputField.value;
+    // added some code from here
+    if(inputField.value.length > 0) {
+      for (let i = 0;i<prompts.length-1;i++) {
+        setList(prompts[i].filter(item => {
+          return item.startsWith(input)
+        }))
+      }
+      if (e.code === "Enter") {
+        inputField.value = "";
+        output(input);
+      }
+    }
+    else {
+      list.innerHTML = ''
+    }
+    // till here 
+  });
+});
+
+function output(input) {
+  let product;
+
+  // Regex remove non word/space chars
+  // Trim trailing whitespce
+  // Remove digits - not sure if this is best
+  // But solves problem of entering something like 'hi1'
+
+  let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
+  text = text
+    .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "")
+    .replace(/r u/g, "are you");
+
+  if (compare(prompts, replies, text)) { 
+    // Search for exact match in `prompts`
+    product = compare(prompts, replies, text);
+  } else if (text.match(/thank/gi)) {
+    product = "You're welcome!"
+  } else if (text.match(/(corona|covid|virus)/gi)) {
+    // If no match, check if message contains `coronavirus`
+    product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
+  } else {
+    // If all else fails: random alternative
+    product = alternative[Math.floor(Math.random() * alternative.length)];
+  }
+
+  // Update DOM
+  addChat(input, product);
+}
+
+function compare(promptsArray, repliesArray, string) {
+  let reply;
+  let replyFound = false;
+  for (let x = 0; x < promptsArray.length; x++) {
+    for (let y = 0; y < promptsArray[x].length; y++) {
+      if (promptsArray[x][y] === string) {
+        let replies = repliesArray[x];
+        reply = replies[Math.floor(Math.random() * replies.length)];
+        replyFound = true;
+        // Stop inner loop when input value matches prompts
+        break;
+      }
+    }
+    if (replyFound) {
+      // Stop outer loop when reply is found instead of interating through the entire array
+      break;
+    }
+  }
+  return reply;
+}
+
+function addChat(input, product) {
+  const messagesContainer = document.getElementById("messages");
+
+  let userDiv = document.createElement("div");
+  userDiv.id = "user";
+  userDiv.className = "user response";
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+  messagesContainer.appendChild(userDiv);
+
+  let botDiv = document.createElement("div");
+  let botImg = document.createElement("img");
+  let botText = document.createElement("span");
+  botDiv.id = "bot";
+  botImg.src = "bot-mini.png";
+  botImg.className = "avatar";
+  botDiv.className = "bot response";
+  botText.innerText = "Typing...";
+  botDiv.appendChild(botText);
+  botDiv.appendChild(botImg);
+  messagesContainer.appendChild(botDiv);
+  // Keep messages at most recent
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+
+  // Fake delay to seem "real"
+  setTimeout(() => {
+    botText.innerText = `${product}`;
+    textToSpeech(product)
+  }, 2000
+  )
+
+}
